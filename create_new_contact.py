3db88035d726221895083704cf68a16d02e42512
The Master <zoom906@gmail.com>
2020-05-02 15:38:09 +0300

    Task 4 for test create_new_contact we add fixture as parameter

--- create_new_contact.py
+++ create_new_contact.py
@@ -1,24 +1,21 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 from new_contact import New_contact
 from application_new_contact import Application_new_contact
 
-class CreateNewContact(unittest.TestCase):
-    def setUp(self):
-        self.app = Application_new_contact()
+@pytest.fixture
+def app(request):
+    fixture = Application_new_contact()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-    def test_create_new_contact(self):
-        self.app.login(username="admin", password="secret")
-        self.app.add_new_contact()
-        self.app.fill_new_form_for_add_new_contact(New_contact(firstname="Alex", middlename="fffefe", lastname="wfawf", nickname="fwwfett", title="wfwfwer", company="erreeg",
+def test_create_new_contact(self):
+    app.login(username="admin", password="secret")
+    app.add_new_contact()
+    app.fill_new_form_for_add_new_contact(New_contact(firstname="Alex", middlename="fffefe", lastname="wfawf", nickname="fwwfett", title="wfwfwer", company="erreeg",
                                                address="fwfawfaf fwfwaf fferwrr", home="wfwff", mobile="89077777777", work="wrefddf", fax="809876",
                                                email="fesefsf@mail.ru", address2="awwaagwg ffeef fefert", phone2="89066666666",
                                                notes="awfawafwa fjfjhw fehgerrt"))
-        self.app.submit_new_contact()
-        self.app.logout()
+    app.submit_new_contact()
+    app.logout()
 
-    def tearDown(self):
-        self.app.destroy()
-
-if __name__ == "__main__":
-    unittest.main()
