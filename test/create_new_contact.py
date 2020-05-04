c9e6884ec7c6a18f4f39831291c5a6aae5b1730d
The Master <zoom906@gmail.com>
2020-05-04 14:44:41 +0300

    Task 5 for the test create_new_contact we defined a class for working with contacts

--- test/create_new_contact.py
+++ test/create_new_contact.py
@@ -12,12 +12,12 @@ def app(request):
 
 def test_create_new_contact(app):
     app.contact_session.login(username="admin", password="secret")
-    app.add_new_contact()
-    app.fill_new_form_for_add_new_contact(
+    app.group_new_contact.add_new_contact()
+    app.group_new_contact.fill_new_form_for_add_new_contact(
         New_contact(firstname="Alex", middlename="fffefe", lastname="wfawf", nickname="fwwfett", title="wfwfwer",
                     company="erreeg",
                     address="fwfawfaf fwfwaf fferwrr", home="wfwff", mobile="89077777777", work="wrefddf", fax="809876",
                     email="fesefsf@mail.ru", address2="awwaagwg ffeef fefert", phone2="89066666666",
                     notes="awfawafwa fjfjhw fehgerrt"))
-    app.submit_new_contact()
+    app.group_new_contact.submit_new_contact()
     app.contact_session.logout()
