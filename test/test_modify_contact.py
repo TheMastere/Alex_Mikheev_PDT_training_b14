ca6787d2ee5698737341b8e435db38b6c5c42f98
The Master <zoom906@gmail.com>
2020-05-05 19:16:33 +0300

    Task 7 add test_modify_contact

---
+++ test/test_modify_contact.py
@@ -0,0 +1,12 @@
+
+from model.group_modify_contact import Modify_contact
+
+def test_modify_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.group_new_contact.modify_contact(
+        Modify_contact(firstname="Niokolay", middlename="ggggg", lastname="Stekolnikov", nickname="Steklov", title="razotr",
+                    company="bichpacket",
+                    address="kifwwf owowif pwofpwf", home="whitehime", mobile="89766666777", work="wiwiwff", fax="97632",
+                    email="jfhwwfiwi@yandex.ru", address2="gwiiree ierjjie oerperpe", phone2="8973333333",
+                    notes="jfkwkjfkw jwfjwfj whfhwfhwfwfiwffw"))
+    app.session.logout()
