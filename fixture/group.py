822d070035964469dd8a6e7e67260666ec58379a
The Master <zoom906@gmail.com>
2020-05-10 15:30:21 +0300

    Task 8 Optimization of data entry in group modification forms

--- fixture/group.py
+++ fixture/group.py
@@ -1,7 +1,6 @@
 
 
 class GroupHelper:
-
     def __init__(self, app):
         self.app = app
 
@@ -22,19 +21,22 @@ class GroupHelper:
     def delete_first_group(self):
         wd = self.app.wd
         self.open_groups_page()
-        # select first group
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_group()
         # submit delete
         wd.find_element_by_name("delete").click()
         self.return_to_group_page()
 
+    def select_first_group(self):
+        wd = self.app.wd
+        wd.find_element_by_name("selected[]").click()
+
     def modify_group(self, group):
         wd = self.app.wd
         self.open_groups_page()
-        # select group
-        wd.find_element_by_name("selected[]").click()
-        # submit to modify group
+        self.select_first_group()
+        # open modification form
         wd.find_element_by_name("edit").click()
+        # fill group form
         self.fill_group_form(group)
         # update group creation
         wd.find_element_by_name("update").click()
@@ -42,16 +44,16 @@ class GroupHelper:
 
     def fill_group_form(self, group):
         wd = self.app.wd
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
+        self.change_field_value("group_name", group.name)
+        self.change_field_value("group_header", group.header)
+        self.change_field_value("group_footer", group.footer)
+
+    def change_field_value(self, field_name, text):
+        wd = self.app.wd
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def return_to_group_page(self):
         wd = self.app.wd
