221dc424f8cf045210227a1a51e7adc410c2dffe
The Master <zoom906@gmail.com>
2020-05-12 21:45:59 +0300

    Task 9 Added tests for deleting and modifying groups and contacts with at least one element pre-test

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,7 +2,17 @@
 from model.group_new_contact import New_contact
 
 def test_modify_contact(app):
-    app.group_new_contact.modify_contact(
+    if app.group_new_contact.count() == 0:
+        app.group_new_contact.add_new_contact()
+        app.group_new_contact.fill_new_form_for_add_new_contact(
+            New_contact(firstname="Alex", middlename="fffefe", lastname="wfawf", nickname="fwwfett", title="wfwfwer",
+                        company="erreeg",
+                        address="fwfawfaf fwfwaf fferwrr", home="wfwff", mobile="89077777777", work="wrefddf",
+                        fax="809876",
+                        email="fesefsf@mail.ru", address2="awwaagwg ffeef fefert", phone2="89066666666",
+                        notes="awfawafwa fjfjhw fehgerrt"))
+        app.group_new_contact.submit_new_contact()
+        app.group_new_contact.modify_contact(
         New_contact(firstname="Niokolay", middlename="ggggg", lastname="Stekolnikov", nickname="Steklov", title="razotr",
                     company="bichpacket",
                     address="kifwwf owowif pwofpwf", home="whitehime", mobile="89766666777", work="wiwiwff", fax="97632",
@@ -10,9 +20,11 @@ def test_modify_contact(app):
                     notes="jfkwkjfkw jwfjwfj whfhwfhwfwfiwffw"))
 
 def test_modify_contact_firstname(app):
-    app.group_new_contact.modify_contact(
+    if app.group_new_contact.count() == 1:
+        app.group_new_contact.modify_contact(
         New_contact(firstname="Koleksandr111"))
 
 def test_modify_contact_address(app):
-    app.group_new_contact.modify_contact(
+    if app.group_new_contact.count() == 1:
+        app.group_new_contact.modify_contact(
         New_contact(address="NOadress nowadhhge g111ert"))
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,10 +2,14 @@
 from model.group import Group
 
 def test_modify_group(app):
-    app.group.modify_group(Group(name="gjjgekglelkr", header="iireeorererp", footer="ijgegegoegoegko"))
+    if app.group.count() == 0:
+        app.group.create(Group(name="testtesttest"))
+        app.group.modify_group(Group(name="gjjgekglelkr", header="iireeorererp", footer="ijgegegoegoegko"))
 
 def test_modify_group_name(app):
-    app.group.modify_group(Group(name="NewNewGroup1"))
+    if app.group.count() == 1:
+        app.group.modify_group(Group(name="NewNewGroup1"))
 
 def test_modify_group_header(app):
-    app.group.modify_group(Group(header="NewHeader111"))
+    if app.group.count() == 1:
+        app.group.modify_group(Group(header="NewHeader111"))
