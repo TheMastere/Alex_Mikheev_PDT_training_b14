29a1134ab0597f03e6d855fe061115294dc6f7af
The Master <zoom906@gmail.com>
2020-05-17 01:02:40 +0300

    Task 11 Group list uploading is implemented

--- fixture/group.py
+++ fixture/group.py
@@ -1,3 +1,4 @@
+from model.group import Group
 
 
 class GroupHelper:
@@ -12,7 +13,7 @@ class GroupHelper:
     def create(self, group):
         wd = self.app.wd
         self.open_groups_page()
-        #init group creation
+        # init group creation
         wd.find_element_by_name("new").click()
         self.fill_group_form(group)
         # submit group creation
@@ -43,7 +44,6 @@ class GroupHelper:
         wd.find_element_by_name("update").click()
         self.return_to_group_page()
 
-
     def fill_group_form(self, group):
         wd = self.app.wd
         self.change_field_value("group_name", group.name)
@@ -65,3 +65,13 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        groups = []
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            groups.append(Group(name=text, id=id))
+        return groups
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,14 +2,23 @@
 from model.group import Group
 
 def test_modify_group(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="testtesttest"))
         app.group.modify_group(Group(name="gjjgekglelkr", header="iireeorererp", footer="ijgegegoegoegko"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_modify_group_name(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 1:
         app.group.modify_group(Group(name="NewNewGroup1"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
 
 def test_modify_group_header(app):
+    old_groups = app.group.get_group_list()
     if app.group.count() == 1:
         app.group.modify_group(Group(header="NewHeader111"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
