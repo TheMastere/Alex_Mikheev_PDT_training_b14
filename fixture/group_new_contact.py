f74b78510a54ab0806c4efc5c5d0caba61842c67
The Master <zoom906@gmail.com>
2020-05-17 15:08:29 +0300

    Task 11 fixed open_home_page method

--- fixture/group_new_contact.py
+++ fixture/group_new_contact.py
@@ -7,7 +7,7 @@ class ContactHelper:
 
     def open_home_page(self):
         wd = self.app.wd
-        if not (wd.current_url == "http://localhost/addressbook/") and (len(wd.find_elements_by_name("add")) > 0):
+        if not (wd.current_url == "http://localhost/addressbook/" and len(wd.find_elements_by_name("add")) > 0):
             wd.find_element_by_link_text("home").click()
 
     def add_new_contact(self):
