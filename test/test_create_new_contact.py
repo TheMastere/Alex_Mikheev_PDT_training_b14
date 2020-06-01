933037cc70a12764c022cca2ed5a8ef009913feb
The Master <zoom906@gmail.com>
2020-06-01 18:17:36 +0300

    Task 15 Tests refined and one cycle created to generate multiple groups

--- test/test_create_new_contact.py
+++ test/test_create_new_contact.py
@@ -6,22 +6,19 @@ import string
 
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + string.punctuation
+    symbols = string.ascii_letters + string.digits + string.punctuation + " " *10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-testdata = [New_contact(firstname=firstname, middlename=middlename, lastname=lastname, address=address,
-                        homephone=homephone, mobilephone=mobilephone, workphone=workphone,
-                        email=email, secondary_phone=secondary_phone)
-            for firstname in ["", random_string("firstname", 10)]
-            for middlename in ["", random_string("middlename", 10)]
-            for lastname in ["", random_string("lastname", 10)]
-            for address in ["", random_string("address", 20)]
-            for homephone in ["", random_string("homephone", 7)]
-            for mobilephone in ["", random_string("mobilephone", 7)]
-            for workphone in ["", random_string("workphone", 7)]
-            for email in ["", random_string("email", 10)]
-            for secondary_phone in ["", random_string("secondary_phone", 7)]]
+testdata = [New_contact(firstname="", middlename="", lastname="", address="",
+                        homephone="", mobilephone="", workphone="",
+                        email="", secondary_phone="")] + [
+               New_contact(firstname=random_string("name", 10), middlename=random_string("name", 10),
+                           lastname=random_string("name", 10), address=random_string("name", 20),
+                           homephone=random_string("name", 7), mobilephone=random_string("name", 7),
+                           workphone=random_string("name", 7),
+                           email=random_string("name", 10), secondary_phone=random_string("name", 7))
+]
 
 @pytest.mark.parametrize("new_contact", testdata, ids=[repr(x) for x in testdata])
 def test_create_new_contact(app, new_contact):
