19c72ea6f3fff5d3380c06e3fb5f401fbdd011bb
The Master <zoom906@gmail.com>
2020-06-09 00:58:34 +0300

    Task 20 Checking the data downloaded from the database. Modifying tests to modify groups.

--- fixture/group.py
+++ fixture/group.py
@@ -70,6 +70,19 @@ class GroupHelper:
         self.return_to_group_page()
         self.group_cache = None
 
+    def modify_group_by_id(self, id, group):
+        wd = self.app.wd
+        self.open_groups_page()
+        self.select_group_by_id(id)
+        # open modification form
+        wd.find_element_by_name("edit").click()
+        # fill group form
+        self.fill_group_form(group)
+        # update group creation
+        wd.find_element_by_name("update").click()
+        self.return_to_group_page()
+        self.group_cache = None
+
     def fill_group_form(self, group):
         wd = self.app.wd
         self.change_field_value("group_name", group.name)
