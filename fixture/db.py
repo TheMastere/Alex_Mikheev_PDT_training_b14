3faaefe7de04579e4bd6c06f428756a6eb6d18ea
The Master <zoom906@gmail.com>
2020-06-09 00:18:29 +0300

    Task 20 Checking the data downloaded from the database. Modified tests for creating and deleting groups.

---
+++ fixture/db.py
@@ -0,0 +1,27 @@
+import pymysql.cursors
+from model.group import Group
+
+class DbFixture:
+
+    def __init__(self, host, port, name, user, password):
+        self.host = host
+        self.port = port
+        self.name = name
+        self.user = user
+        self.password = password
+        self.connection = pymysql.connect(host=host, port=port, database=name, user=user, password=password, autocommit=True)
+
+    def get_group_list(self):
+        list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+            for row in cursor:
+                (id, name, header, footer) = row
+                list.append(Group(id=str(id), name=name, header=header, footer=footer))
+        finally:
+            cursor.close()
+        return list
+
+    def destroy(self):
+        self.connection.close()
