093cd58eb13f59e7b7f25cba347eb28ed012ece4
The Master <zoom906@gmail.com>
2020-06-10 10:17:50 +0300

    Task 20 Implemented comparison of contacts loaded from the database with contacts loaded from the user interface

--- fixture/db.py
+++ fixture/db.py
@@ -1,4 +1,5 @@
 import pymysql.cursors
+from model.contact import New_contact
 from model.group import Group
 
 class DbFixture:
@@ -12,16 +13,29 @@ class DbFixture:
         self.connection = pymysql.connect(host=host, port=port, database=name, user=user, password=password, autocommit=True)
 
     def get_group_list(self):
-        list = []
+        group_list = []
         cursor = self.connection.cursor()
         try:
             cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
             for row in cursor:
                 (id, name, header, footer) = row
-                list.append(Group(id=str(id), name=name, header=header, footer=footer))
+                group_list.append(Group(id=str(id), name=name, header=header, footer=footer))
         finally:
             cursor.close()
-        return list
+        return group_list
+
+    def get_contact_list(self):
+        contact_list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select id, firstname, lastname, middlename, company, address, home, mobile, work, email from addressbook")
+            for row in cursor:
+                (id, firstname, lastname, middlename, company, address, home, mobile, work, email) = row
+                contact_list.append(New_contact(id=str(id), firstname=firstname, lastname=lastname, middlename=middlename, company=company,
+                                  address=address, homephone=home, mobilephone=mobile, workphone=work, email=email))
+        finally:
+            cursor.close()
+        return contact_list
 
     def destroy(self):
         self.connection.close()
--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,4 +1,6 @@
 from model.group import Group
+from model.contact import New_contact
+
 
 def test_group_list(app, db):
     ui_list = app.group.get_group_list()
@@ -6,3 +8,10 @@ def test_group_list(app, db):
         return Group(id=group.id, name=group.name.strip())
     db_list = map(clean, db.get_group_list())
     assert sorted(ui_list, key=Group.id_or_max) == sorted(db_list, key=Group.id_or_max)
+
+def test_contact_list(app, db):
+    ui_list = app.group_new_contact.get_contact_list()
+    def clean(group_new_contact):
+        return New_contact(id=group_new_contact.id, firstname=group_new_contact.firstname.strip(), lastname=group_new_contact.lastname.strip())
+    db_list = map(clean, db.get_contact_list())
+    assert sorted(ui_list, key=New_contact.id_or_max) == sorted(db_list, key=New_contact.id_or_max)
